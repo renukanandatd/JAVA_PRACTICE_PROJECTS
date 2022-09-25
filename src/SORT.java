08486c10b876232cd7ce62e2f349389d7f4bd4e7
renukanandatd <renukananda2000@gmail.com>
2022-09-25 08:56:39 +0530

    second commit

--- src/SORT.java
+++ src/SORT.java
@@ -1,20 +1,21 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class SORT {
     public static void main(String[] args) {
+
         String path = "C:\\Users\\Renukananda T D\\IdeaProjects\\SORT\\src\\SORT - Sheet1.csv";
-        String line = "";
-        List<String> list= new ArrayList<>();
+        String line;
+        ArrayList<String> list= new ArrayList<>();
+
         try{
             BufferedReader br = new BufferedReader(new FileReader(path));
             PrintWriter pr = new PrintWriter("SORTED");
                 while ((line = br.readLine()) != null) {
                     String[] values = line.split(",");
-                    list.add(values[0]);
+                    list.add(values[2]);
                 }
                 list.remove(0);
                 Collections.sort(list);
@@ -23,9 +24,7 @@ public class SORT {
                 }
                 pr.close();
         }
-        catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
+
         catch(IOException e){
             e.printStackTrace();
         }
